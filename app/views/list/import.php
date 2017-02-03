195adb437dd85b2081e5ba3146ae6c2661cc0d6b
Joshua Parker <joshmac3@icloud.com>
2017-02-02 23:52:04 -0500

    Added csv template for download.

--- app/views/list/import.php
+++ app/views/list/import.php
@@ -32,29 +32,52 @@ define('SCREEN', 'lists');
     <!-- Main content -->
     <section class="content">
 
-        <?= _tc_flash()->showMessage(); ?> 
+        <?= _tc_flash()->showMessage(); ?>
+        
+        <div class="alert alert-info" style="font-size:1.2em;text-align:center;color:#333333 !important;"><?=_t('Click "Download Template" for import example. Fields in order are first name, last name, email address.');?></div>
 
         <!-- SELECT2 EXAMPLE -->
         <div class="box box-default">
-            <div class="box-body">
-                    <!-- form start -->
-                    <form role="form" method="post" action="<?= get_base_url(); ?>list/<?=_h($list->id);?>/import/" enctype="multipart/form-data">
-                        <div class="box-body">
-                            <div class="form-group">
-                                <label for="exampleInputFile"><?= _t('File input'); ?></label>
-                                <input type="file" name="csv_import" required/>
+            <!-- form start -->
+            <form role="form" method="post" action="<?= get_base_url(); ?>list/<?= _h($list->id); ?>/import/" enctype="multipart/form-data">
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label><font color="red">*</font> <?= _t('Delimiter'); ?></label>
+                                    <select class="form-control select2" name="delimiter" style="width: 100%;" required>
+                                        <option>&nbsp;</option>
+                                        <option value="del1">, <?= _t('(Comma)'); ?></option>
+                                        <option value="del2">; <?= _t('(Semicolon)'); ?></option>
+                                        <option value="del3"> <?= _t('(Line Break'); ?></option>
+                                        <option value="del4"> <?= _t('(TAB)'); ?></option>
+                                    </select>
+                                    <p class="help-block"><?= _t('If Closed, no one will be able to subscribe to the list.'); ?></p>
+                                </div>
                             </div>
-                            <!-- /.box-body -->
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-primary"><?= _t('Submit'); ?></button>
-                                <button type="button" class="btn btn-primary" onclick="window.location='<?=get_base_url();?>list/'"><?= _t('Cancel'); ?></button>
-                            </div>
+                            <!-- /.box -->
                         </div>
-                        <!-- /.box -->
-                    </form>
-                    <!-- form end -->
-            </div>
-            <!-- /.box-body -->
+                        
+                        <div class="col-md-6">
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label for="exampleInputFile"><font color="red">*</font> <?= _t('File input'); ?></label>
+                                    <input type="file" name="csv_import" required/>
+                                </div>
+                            </div>
+                            <!-- /.box -->
+                            <!-- form end -->
+                        </div>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary"><?= _t('Submit'); ?></button>
+                        <button type="button" class="btn btn-primary" onclick="window.location = '<?= get_base_url(); ?>list/download/?f=tinyCampaign.csv'"><?= _t('Download Template'); ?></button>
+                        <button type="button" class="btn btn-primary" onclick="window.location = '<?= get_base_url(); ?>list/'"><?= _t('Cancel'); ?></button>
+                    </div>
+            </form>
         </div>
         <!-- /.box -->
 
