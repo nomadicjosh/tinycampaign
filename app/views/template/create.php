40f52930c90d06560788f81fa196c21de6cfd50a
Joshua Parker <joshmac3@icloud.com>
2017-02-04 17:12:49 -0500

    Added ability to add and use templates.

---
+++ app/views/template/create.php
@@ -0,0 +1,198 @@
+<?php
+if (!defined('BASE_PATH'))
+    exit('No direct script access allowed');
+/**
+ * Create Template View
+ *  
+ * @license GPLv3
+ * 
+ * @since       2.0.1
+ * @package     tinyCampaign
+ * @author      Joshua Parker <joshmac3@icloud.com>
+ */
+$app = \Liten\Liten::getInstance();
+$app->view->extend('_layouts/dashboard');
+$app->view->block('dashboard');
+define('SCREEN_PARENT', 'tpls');
+define('SCREEN', 'ctpl');
+
+?>
+
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+<script type="text/javascript">
+    /////////////
+    // TINYMCE //
+    /////////////
+
+    // Initialize TinyMCE
+    tinyMCE.init({
+        theme: 'modern',
+        mode: 'specific_textareas',
+        editor_selector: 'template',
+        elements: 'message',
+        height: '350',
+        autosave_ask_before_unload: false,
+        relative_urls: false,
+        remove_script_host: false,
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code",
+            "insertdatetime media table contextmenu paste"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | placeholder",
+        file_picker_callback: elFinderBrowser,
+        setup: function (editor) {
+            editor.addButton('placeholder', {
+                type: 'menubutton',
+                text: 'Email Placeholders',
+                icon: false,
+                menu: [
+                    {text: 'Date', onclick: function () {
+                            editor.insertContent('{todays_date}');
+                        }},
+                    {text: 'View Online', onclick: function () {
+                            editor.insertContent('{view_online}');
+                        }},
+                    {text: 'First Name', onclick: function () {
+                            editor.insertContent('{first_name}');
+                        }},
+                    {text: 'Last Name', onclick: function () {
+                            editor.insertContent('{last_name}');
+                        }},
+                    {text: 'Subscriber Email', onclick: function () {
+                            editor.insertContent('{email}');
+                        }},
+                    {text: 'Address1', onclick: function () {
+                            editor.insertContent('{address1}');
+                        }},
+                    {text: 'Address2', onclick: function () {
+                            editor.insertContent('{address2}');
+                        }},
+                    {text: 'City', onclick: function () {
+                            editor.insertContent('{city}');
+                        }},
+                    {text: 'State', onclick: function () {
+                            editor.insertContent('{state}');
+                        }},
+                    {text: 'Postal Code', onclick: function () {
+                            editor.insertContent('{postal_code}');
+                        }},
+                    {text: 'Country', onclick: function () {
+                            editor.insertContent('{country}');
+                        }},
+                    {text: 'Unsubscribe URL', onclick: function () {
+                            editor.insertContent('{unsubscribe_url}');
+                        }},
+                    {text: 'Subscriber Preferences', onclick: function () {
+                            editor.insertContent('{personal_preferences}');
+                        }}
+                ]
+            });
+        }
+    });
+    function elFinderBrowser(callback, value, meta) {
+        tinymce.activeEditor.windowManager.open({
+            file: '<?= get_base_url(); ?>campaign/elfinder/', // use an absolute path!
+            title: 'elFinder 2.0',
+            width: 900,
+            height: 600,
+            resizable: 'yes'
+        }, {
+            oninsert: function (file) {
+                // Provide file and text for the link dialog
+                if (meta.filetype == 'file') {
+//            callback('mypage.html', {text: 'My text'});
+                    callback(file.url);
+                }
+
+                // Provide image and alt text for the image dialog
+                if (meta.filetype == 'image') {
+//            callback('myimage.jpg', {alt: 'My alt text'});
+                    callback(file.url);
+                }
+
+                // Provide alternative source and posted for the media dialog
+                if (meta.filetype == 'media') {
+//            callback('movie.mp4', {source2: 'alt.ogg', poster: 'image.jpg'});
+                    callback(file.url);
+                }
+            }
+        });
+        return false;
+    }
+</script>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1><?= _t('Create Template'); ?></h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= get_base_url(); ?>dashboard/"><i class="fa fa-dashboard"></i> <?= _t('Dashboard'); ?></a></li>
+            <li><a href="<?= get_base_url(); ?>template/"><i class="fa fa-file"></i> <?= _t('Templates'); ?></a></li>
+            <li class="active"><?= _t('Create Template'); ?></li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <?= _tc_flash()->showMessage(); ?>
+        
+        <div class="box box-default">
+            <!-- form start -->
+            <form method="post" action="<?= get_base_url(); ?>template/create/" data-toggle="validator" autocomplete="off">
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label><font color="red">*</font> <?= _t('Name'); ?></label>
+                                <input type="text" class="form-control" name="name" value="<?=(_h($app->req->post['name']) != '' ? _h($app->req->post['name']) : '');?>" required/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label><?= _t('Short Description'); ?></label>
+                                <textarea class="form-control" rows="3" name="description"><?=(_h($app->req->post['description']) != '' ? _h($app->req->post['description']) : '');?></textarea>
+                            </div>
+
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label><?= _t('Created'); ?></label>
+                                <input type="text" class="form-control" value="<?= Jenssegers\Date\Date::now()->format('M. d, Y @ h:i A');?>" readonly/>
+                            </div>
+
+                        </div>
+                        <!-- /.col -->
+
+                        <div class="col-md-12">
+
+                            <div class="form-group">
+                                <label><font color="red">*</font> <?= _t('HTML Message'); ?></label>
+                                <textarea class="form-control template" rows="3" name="content" required><?=(_h($app->req->post['content']) != '' ? _h($app->req->post['content']) : '');?></textarea>
+                            </div>
+
+                        </div>
+                        <!-- /.col -->
+
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary"><?= _t('Submit'); ?></button>
+                    <button type="button" class="btn btn-primary" onclick="window.location='<?=get_base_url();?>template/'"><?=_t( 'Cancel' );?></button>
+                </div>
+            </form>
+            <!-- form end -->
+        </div>
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php $app->view->stop(); ?>
