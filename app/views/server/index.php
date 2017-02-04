bb8b461ecfa9d75bc30622b2923d27097088f17a
Joshua Parker <joshmac3@icloud.com>
2017-02-04 17:12:24 -0500

    Added multiple smtp server capability.

---
+++ app/views/server/index.php
@@ -0,0 +1,106 @@
+<?php
+if (!defined('BASE_PATH'))
+    exit('No direct script access allowed');
+/**
+ * Servers List View
+ *  
+ * @license GPLv3
+ * 
+ * @since       2.0.1
+ * @package     tinyCampaign
+ * @author      Joshua Parker <joshmac3@icloud.com>
+ */
+$app = \Liten\Liten::getInstance();
+$app->view->extend('_layouts/dashboard');
+$app->view->block('dashboard');
+define('SCREEN_PARENT', 'servers');
+define('SCREEN', 'server');
+
+?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1><?= _t('Servers'); ?></h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= get_base_url(); ?>dashboard/"><i class="fa fa-dashboard"></i> <?= _t('Dashboard'); ?></a></li>
+            <li class="active"><?= _t('Servers'); ?></li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <?= _tc_flash()->showMessage(); ?> 
+
+        <!-- SELECT2 EXAMPLE -->
+        <div class="box box-default">
+            <div class="box-body">
+                <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th class="text-center"><?= _t('Name'); ?></th>
+                            <th class="text-center"><?= _t('Created'); ?></th>
+                            <th class="text-center"><?= _t('Action'); ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($servers as $server) : ?>
+                            <tr class="gradeX">
+                                <td class="text-center"><?= _h($server->name); ?></td>
+                                <td class="text-center"><?= _h($server->addDate); ?></td>
+                                <td class="text-center">
+                                    <a href="<?= get_base_url(); ?>server/<?= _h($server->id); ?>/" data-toggle="tooltip" data-placement="top" title="View/Edit"><button class="btn bg-yellow"><i class="fa fa-edit"></i></button></a>
+                                    <a<?=ae('delete_campaign');?> href="#" data-toggle="modal" data-target="#delete-<?= _h($server->id); ?>"><button class="btn bg-red"><i class="fa fa-trash-o"></i></button></a>
+                                    
+                                    <!-- modal -->
+                                    <div class="modal" id="delete-<?= _h($server->id); ?>">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title"><?= _h($server->name); ?></h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p><?=_t('Are you sure you want to delete this server.?');?></p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><?= _t('Close'); ?></button>
+                                                    <button type="button" class="btn btn-primary" onclick="window.location='<?=get_base_url();?>server/<?= _h($server->id); ?>/d/'"><?= _t('Confirm'); ?></button>
+                                                </div>
+                                            </div>
+                                            <!-- /.modal-content -->
+                                        </div>
+                                        <!-- /.modal-dialog -->
+                                    </div>
+                                    <!-- /.modal -->
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th class="text-center"><?= _t('Name'); ?></th>
+                            <th class="text-center"><?= _t('Created'); ?></th>
+                            <th class="text-center"><?= _t('Action'); ?></th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php $app->view->stop(); ?>
