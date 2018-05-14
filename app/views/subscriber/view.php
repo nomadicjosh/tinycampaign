4f5522ef83ca6232462c1cb7a7bd9109704c9f7e
Joshua Parker <joshmac3@icloud.com>
2018-05-14 19:59:15 -0400

    Added missing exception field to forms.

--- app/views/subscriber/view.php
+++ app/views/subscriber/view.php
@@ -38,38 +38,46 @@ $tags = "{tag: '".implode("'},{tag: '", get_subscriber_tag_list())."'}";
         <!-- SELECT2 EXAMPLE -->
         <div class="box box-default">
             <!-- form start -->
-            <form method="post" action="<?= get_base_url(); ?>subscriber/<?=_h((int)$subscriber->id);?>/" data-toggle="validator" autocomplete="off">
+            <form method="post" action="<?= get_base_url(); ?>subscriber/<?=_escape((int)$subscriber->id);?>/" data-toggle="validator" autocomplete="off">
                 <div class="box-body">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label><?= _t('First Name'); ?></label>
-                                <input type="text" class="form-control" name="fname" value="<?=_h($subscriber->fname);?>" />
+                                <input type="text" class="form-control" name="fname" value="<?=_escape($subscriber->fname);?>" />
                             </div>
 
                             <div class="form-group">
                                 <label><?= _t('Last Name'); ?></label>
-                                <input type="text" class="form-control" name="lname" value="<?=_h($subscriber->lname);?>" />
+                                <input type="text" class="form-control" name="lname" value="<?=_escape($subscriber->lname);?>" />
                             </div>
                             
                             <div class="form-group">
                                 <label><font color="red">*</font> <?= _t('Email'); ?></label>
-                                <input type="email" class="form-control" name="email" value="<?=_h($subscriber->email);?>" required/>
+                                <input type="email" class="form-control" name="email" value="<?=_escape($subscriber->email);?>" required/>
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('Address 1'); ?></label>
-                                <input type="text" class="form-control" name="address1" value="<?=_h($subscriber->address1);?>" />
+                                <input type="text" class="form-control" name="address1" value="<?=_escape($subscriber->address1);?>" />
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('Address 2'); ?></label>
-                                <input type="text" class="form-control" name="address2" value="<?=_h($subscriber->address2);?>" />
+                                <input type="text" class="form-control" name="address2" value="<?=_escape($subscriber->address2);?>" />
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('City'); ?></label>
-                                <input type="text" class="form-control" name="city" value="<?=_h($subscriber->city);?>" />
+                                <input type="text" class="form-control" name="city" value="<?=_escape($subscriber->city);?>" />
+                            </div>
+                            
+                            <div class="form-group">
+                                <label><?= _t('State'); ?></label>
+                                <select class="form-control select2" name="state" style="width: 100%;">
+                                    <option value="NULL">&nbsp;</option>
+                                    <?php table_dropdown('state','code <> "NULL"','code','code','name',_escape($subscriber->state)); ?>
+                                </select>
                             </div>
                             
                         </div>
@@ -77,34 +85,38 @@ $tags = "{tag: '".implode("'},{tag: '", get_subscriber_tag_list())."'}";
                         <div class="col-md-6">
                             
                             <div class="form-group">
-                                <label><?= _t('State'); ?></label>
-                                <select class="form-control select2" name="state" style="width: 100%;">
-                                    <option value="NULL">&nbsp;</option>
-                                    <?php table_dropdown('state','code <> "NULL"','code','code','name',_h($subscriber->state)); ?>
-                                </select>
-                            </div>
-                            
-                            <div class="form-group">
                                 <label><?= _t('Postal Code'); ?></label>
-                                <input type="text" class="form-control" name="postal_code" value="<?=_h($subscriber->postal_code);?>" />
+                                <input type="text" class="form-control" name="postal_code" value="<?=_escape($subscriber->postal_code);?>" />
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('Country'); ?></label>
                                 <select class="form-control select2" name="country" style="width: 100%;">
                                     <option value="NULL">&nbsp;</option>
-                                    <?php table_dropdown('country', 'iso2 <> "NULL"', 'iso2', 'iso2', 'short_name', _h($subscriber->country)); ?>
+                                    <?php table_dropdown('country', 'iso2 <> "NULL"', 'iso2', 'iso2', 'short_name', _escape($subscriber->country)); ?>
                                 </select>
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('Tags'); ?></label>
-                                <input type="text" id="input-tags" name="tags" value="<?=_h($subscriber->tags);?>" />
+                                <input type="text" id="input-tags" name="tags" value="<?=_escape($subscriber->tags);?>" />
+                            </div>
+                            
+                            <div class="form-group">
+                                <label><?= _t('Spammer'); ?>  <a href="#spammer" data-toggle="modal"><span class="badge"><i class="fa fa-question"></i></span></a></label>
+                                <input type="hidden" name="spammer" value="0" />
+                                <input type="checkbox" name="spammer" class="minimal-red" value="1" <?= checked('1', _escape($subscriber->spammer), false);?>/>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label><?= _t('Exception'); ?>  <a href="#exception" data-toggle="modal"><span class="badge"><i class="fa fa-question"></i></span></a></label>
+                                <input type="hidden" name="exception" value="0" />
+                                <input type="checkbox" name="exception" class="flat-red" value="1" <?= checked('1', _escape($subscriber->exception), false);?>/>
                             </div>
                             
                             <div class="form-group">
                                 <label><?= _t('Lists'); ?></label><br />
-                                <ul><?php get_subscription_email_lists(_h((int)$subscriber->id)); ?></ul>
+                                <ul><?php get_subscription_email_lists(_escape((int)$subscriber->id)); ?></ul>
                             </div>
                             
                         </div>
@@ -124,6 +136,51 @@ $tags = "{tag: '".implode("'},{tag: '", get_subscriber_tag_list())."'}";
 
     </section>
     <!-- /.content -->
+    
+    <!-- modal -->
+    <div class="modal" id="spammer">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><?=_t( 'Spammer' );?></h4>
+                </div>
+                <div class="modal-body">
+                    <p><?=_t( "The system will do an automatic check to see if email is to be marked as a spammer. If subscriber is not a spammer and should receive all emails, then check out the Exception option. Or if you feel the subscriber is a spammer, then use this option to mark the subscriber as so." );?></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><?= _t('Close'); ?></button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
+    
+    <!-- modal -->
+    <div class="modal" id="exception">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><?=_t( 'Exception' );?></h4>
+                </div>
+                <div class="modal-body">
+                    <p><?=_t( "If user keeps getting marked as spam but should receive emails nonetheless, check this option." );?></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><?= _t('Close'); ?></button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
+    
 </div>
 <script src="static/assets/js/selectize/js/standalone/selectize.min.js" type="text/javascript"></script>
 <script type="text/javascript">
